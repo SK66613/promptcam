e14f7a62f17272e024445f84e06ee2dc166cbd04
SK66613 <cyberian13@icloud.com>
2026-09-05 22:22:58 +0300

    Load focused v45 shell and library modules

---
+++ web/ui-v45.js
@@ -0,0 +1,17 @@
+(() => {
+  'use strict';
+
+  const modules = [
+    ['/ui-shell-v45.js?v=45', 'data-promptcam-ui-shell-v45'],
+    ['/library-explain-v45.js?v=45', 'data-promptcam-library-explain-v45']
+  ];
+
+  for (const [src, attribute] of modules) {
+    if (document.querySelector(`script[${attribute}]`)) continue;
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    script.setAttribute(attribute, 'true');
+    document.head.append(script);
+  }
+})();
